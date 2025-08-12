8e956e64b168a68d70447cd24a871071b2489166
Etienne <etienne.pautet@gmail.com>
2025-08-11 21:03:38 -0300

    Better key mapping for copilot

--- lua/eti/plugins/copilot.lua
+++ lua/eti/plugins/copilot.lua
@@ -6,8 +6,34 @@ return {
       { "nvim-lua/plenary.nvim", branch = "master" },
     },
     build = "make tiktoken",
-    opts ={
-
+    opts = {
+      -- Your existing opts here
     },
+    config = function(_, opts)
+      require("CopilotChat").setup(opts)
+      
+      -- Quick toggle for CopilotChat window
+      vim.keymap.set("n", "<leader>cc", function()
+        require("CopilotChat").toggle()
+      end, { desc = "Toggle CopilotChat" })
+      
+      -- Toggle autocomplete (GitHub Copilot suggestions)
+      vim.keymap.set("n", "<leader>ca", function()
+        if vim.g.copilot_enabled == nil or vim.g.copilot_enabled == 1 then
+          vim.cmd("Copilot disable")
+          vim.g.copilot_enabled = 0
+          print("Copilot autocomplete disabled")
+        else
+          vim.cmd("Copilot enable")
+          vim.g.copilot_enabled = 1
+          print("Copilot autocomplete enabled")
+        end
+      end, { desc = "Toggle Copilot autocomplete" })
+      
+      -- Alternative: More explicit toggle with status checking
+      vim.keymap.set("n", "<leader>ct", function()
+        vim.cmd("Copilot status")
+      end, { desc = "Check Copilot status" })
+    end,
   },
 }
