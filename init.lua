0f23826ff68d55633c1eff30e5d6b8dcd993b0c3
Etienne <etienne.pautet@gmail.com>
2025-08-12 22:49:35 -0300

    Navigating up and down now centers the page

--- init.lua
+++ init.lua
@@ -1,2 +1,8 @@
 require("eti.core")
 require("eti.lazy")
+
+vim.keymap.set('n', '<C-u>', '<C-u>zz')
+vim.keymap.set('n', '<C-d>', '<C-d>zz')
+
+vim.keymap.set('n', 'n', 'nzz')
+vim.keymap.set('n', 'N', 'Nzz')
