c48b267f82742eccec956981639e51b486e7ef31
Etienne <etienne.pautet@gmail.com>
2025-08-13 13:58:44 -0300

    Added zen and twilight plugins and changed toggle zen to "<leader>zz"

--- init.lua
+++ init.lua
@@ -1,8 +1,15 @@
 require("eti.core")
 require("eti.lazy")
 
+-- neovim
 vim.keymap.set('n', '<C-u>', '<C-u>zz')
 vim.keymap.set('n', '<C-d>', '<C-d>zz')
 
 vim.keymap.set('n', 'n', 'nzz')
 vim.keymap.set('n', 'N', 'Nzz')
+
+
+-- zen mode 
+vim.keymap.set("n", "<Space>zz", function()
+  require("zen-mode").toggle()
+end, { desc = "Toggle Zen Mode" })
