49780184ad5b0ba4ee1db1839e4e0dce9983c3e1
Etienne <etienne.pautet@gmail.com>
2025-08-14 12:27:06 -0300

    Removed autocompletion from copilot

--- lua/eti/plugins/copilot.lua
+++ lua/eti/plugins/copilot.lua
@@ -2,7 +2,6 @@ return {
   {
     "CopilotC-Nvim/CopilotChat.nvim",
     dependencies = {
-      { "github/copilot.vim" },
       { "nvim-lua/plenary.nvim", branch = "master" },
     },
     build = "make tiktoken",
@@ -18,27 +17,7 @@ return {
       vim.keymap.set("n", "<leader>cc", function()
         require("CopilotChat").toggle()
       end, { desc = "Toggle CopilotChat" })
-      
-      vim.g.copilot_auto_trigger = false
-      -- Toggle autocomplete (GitHub Copilot suggestions)
-      vim.keymap.set("n", "<C-c>", function()
-        if vim.g.copilot_enabled == nil or vim.g.copilot_enabled == 1 then
-          vim.cmd("Copilot disable")
-          vim.g.copilot_enabled = 0
-          print("Copilot autocomplete disabled")
-        else
-          vim.cmd("Copilot enable")
-          vim.g.copilot_enabled = 1
-          print("Copilot autocomplete enabled")
-        end
-      end, { desc = "Toggle Copilot autocomplete" })
-      
-      -- Alternative: More explicit toggle with status checking
-      vim.keymap.set("n", "<leader>ct", function()
-        vim.cmd("Copilot status")
-      end, { desc = "Check Copilot status" })
     end,
   },
 }
-
 
